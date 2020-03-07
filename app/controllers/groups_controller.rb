7d0e01046bf8b10e750643a5eec14fee1f610a72
naoya-uchiyama <naoya.uchiyama55@gmail.com>
2020-03-07 09:11:52 +0900

    master

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,7 @@
 class GroupsController < ApplicationController
-
   
   def index
   end
-
 
   def new
     @group = Group.new
@@ -21,12 +19,13 @@ class GroupsController < ApplicationController
 
   def edit
     @group = Group.find(params[:id])
+    
   end
 
   def update
     @group = Group.find(params[:id])
     if @group.update(group_params)
-      redirect_to root_path, notice: 'グループを更新しました'
+      redirect_to group_messages_path(@group), notice: 'グループを更新しました'
     else
       render :edit
     end
